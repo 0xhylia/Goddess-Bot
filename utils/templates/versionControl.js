7b4272a3b44dba9d401d570c653e05f35f6e05d8
Hayden <zzerzv_dev@outlook.com>
2022-11-25 20:06:40 -0500

    Create versionControl.js

---
+++ utils/templates/versionControl.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "5.1.2": {
+        updateNumber: "5.1.2",
+        title: `Update ${this.updateNumber}`,
+        description: "This update is a small update that fixes a few bugs and adds a few new features.",
+        features: [
+            "Added a new command, `/news :query:`",
+            "Removed the ``Premium Nickname`` feature",
+            "Fixed a bug where the ``/ticket`` command would not work",
+        ],
+        updateAuthor: {
+            name: "Akeno",
+            id: "547923574833545226",
+            avatar: "https://cdn.discordapp.com/avatars/547923574833545226/a_98fba18d0511f3a95eb508eafa207eef.gif",
+        },
+        _date: "2022-25-11",
+    }
+}
