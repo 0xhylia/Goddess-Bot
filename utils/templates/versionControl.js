b76fcb30e7f7d27db029f76251377947b1161bfe
Ly <zzerzv_dev@outlook.com>
2022-11-30 12:36:27 -0500

    Update versionControl.js

--- utils/templates/versionControl.js
+++ utils/templates/versionControl.js
@@ -1,18 +1,14 @@
 module.exports = {
-    "5.1.2": {
-        updateNumber: "5.1.2",
-        title: `Update ${this.updateNumber}`,
-        description: "This update is a small update that fixes a few bugs and adds a few new features.",
+        updateNumber: "5.1.4",
+        title: `Version Control Hotfix`,
+        description: "<:5813discordcategory:1047563076725117028> This update is a **SMALL** update with the [``version control``](https://github.com/Red-Cafe/Goddess-Bot/blob/main/utils/templates/versionControl.js)",
         features: [
-            "Added a new command, `/news :query:`",
-            "Removed the ``Premium Nickname`` feature",
-            "Fixed a bug where the ``/ticket`` command would not work",
+            "Fixed the ``version`` finder.",
         ],
         updateAuthor: {
             name: "Akeno",
             id: "547923574833545226",
             avatar: "https://cdn.discordapp.com/avatars/547923574833545226/a_98fba18d0511f3a95eb508eafa207eef.gif",
         },
-        _date: "2022-25-11",
-    }
+        _date: "2022-30-11",
 }
